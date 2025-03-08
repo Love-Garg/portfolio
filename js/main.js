3348ccdac82f0f3af6a50422d60198c98f9fe323
Love-Garg <73755234+Love-Garg@users.noreply.github.com>
2025-03-08 17:22:41 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -218,51 +218,43 @@
 
 	/* local validation */
 	$('#contactForm').validate({
-
-		/* submit via ajax */
 		submitHandler: function(form) {
-
-			var sLoader = $('#submit-loader');
-
-			$.ajax({      	
-
-		      type: "POST",
-		      url: "inc/sendEmail.php",
-		      data: $(form).serialize(),
-		      beforeSend: function() { 
-
-		      	sLoader.fadeIn(); 
-
-		      },
-		      success: function(msg) {
-
-	            // Message was sent
-	            if (msg == 'OK') {
-	            	sLoader.fadeOut(); 
-	               $('#message-warning').hide();
-	               $('#contactForm').fadeOut();
-	               $('#message-success').fadeIn();   
-	            }
-	            // There was an error
-	            else {
-	            	sLoader.fadeOut(); 
-	               $('#message-warning').html(msg);
-		            $('#message-warning').fadeIn();
-	            }
-
-		      },
-		      error: function() {
-
-		      	sLoader.fadeOut(); 
-		      	$('#message-warning').html("Something went wrong. Please try again.");
-		         $('#message-warning').fadeIn();
-
-		      }
-
-	      });     		
-  		}
-
-	});
+		  var sLoader = $('#submit-loader');
+		  var formData = {
+			contactName: $('#contactName').val(),
+			contactEmail: $('#contactEmail').val(),
+			contactSubject: $('#contactSubject').val(),
+			contactMessage: $('#contactMessage').val()
+		  };
+	  
+		  $.ajax({
+			type: "POST",
+			url: "https://script.google.com/macros/s/AKfycbwo05e3yevT8JV2_lkZMKRZY7b8JSuDqLMODEoNUVDnXa9rWn6eL7Mw1CM4GR6_wCqn1w/exec",
+			data: JSON.stringify(formData),
+			contentType: "application/json",
+			beforeSend: function() {
+			  sLoader.fadeIn();
+			},
+			success: function(response) {
+			  sLoader.fadeOut();
+			  if (response.result === 'success') {
+				$('#message-warning').hide();
+				$('#contactForm').fadeOut();
+				$('#message-success').fadeIn();
+			  } else {
+				$('#message-warning').html("An error occurred. Please try again.");
+				$('#message-warning').fadeIn();
+			  }
+			},
+			error: function() {
+			  sLoader.fadeOut();
+			  $('#message-warning').html("Something went wrong. Please try again.");
+			  $('#message-warning').fadeIn();
+			}
+		  });
+		}
+	  });
+	  
 
 
  	/*----------------------------------------------------- */
